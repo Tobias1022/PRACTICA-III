1843be86590bf07ec3dd305a67c7cd831f8d6b67
tobias1022 <tobiasvalenzuela63@gmail.com>
2024-05-20 16:32:40 -0300

    terminado

--- ley_ohm/main.js
+++ ley_ohm/main.js
@@ -1,9 +1,18 @@
-function calcular(s1){
+function calcular() {
+    const operacion = document.getElementById("operacion").value;
+    const valor1 = parseFloat(document.getElementById("valor1").value);
+    const valor2 = parseFloat(document.getElementById("valor2").value);
+    let resultado;
 
+    if (operacion === "corriente") {
+        resultado = valor2 / valor1;
+    } else if (operacion === "tension") {
+        resultado = valor1 * valor2;
+    } else if (operacion === "resistencia") {
+        resultado = valor1 / valor2;
+    }
+
+    document.getElementById("resultado").innerText = `El resultado es: ${resultado}`;
 }
 
-const btn = document.getElementById("btn_ejecutar")
-btn.addEventListener("click",ejecutar)
-function cambiar_label(){
-
-}
+document.getElementById("btn_ejecutar").addEventListener("click", calcular);
